e000c00c10d0c2340a3a0e6ba28503ebf943ba7d
Zahar-Write-Code <tolst.0.2.5.0.2.5@gmail.com>
2023-06-04 10:57:10 +0300

    add fix

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,51 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   private final SessionFactory sessionFactory;
+
+   public UserDaoImp(SessionFactory sessionFactory) {
+      this.sessionFactory = sessionFactory;
+   }
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+
+   public List<User> listUsers() {
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   public User findOwnerByCar(String car_model, int car_series) {
+      EntityManager entityManager = sessionFactory.createEntityManager();
+      try {
+         TypedQuery<User> query = entityManager
+                 .createQuery("SELECT u FROM User u JOIN u.car m WHERE m.model = :model AND m.series =: series", User.class)
+                 .setParameter("model", car_model)
+                 .setParameter("series", car_series);
+         List<User> userList = query.getResultList();
+         if (!userList.isEmpty()) {
+            return userList.get(0);
+         }
+      } finally {
+         entityManager.close();
+      }
+      return new User();
+   }
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,63 @@
+package hiber.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GenerationType;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String model;
+
+    private int series;
+
+    @OneToOne(mappedBy = "car", fetch = FetchType.LAZY)
+    private User owner;
+
+    public Car() {
+
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "id = " + id + ' ' + model + ' ' + series;
+    }
+
+}
